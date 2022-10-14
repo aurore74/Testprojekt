46b4c848180b0071b26d8a46197421a24599afbd
Kouekem Ursule Aurore <ursulekouekem@gmail.com>
2022-10-14 12:44:45 +0200

    seul commit

--- Testprojekt/src/soleil.java
+++ Testprojekt/src/soleil.java
@@ -1,10 +1,1 @@
-
-public class soleil {
-	
-public static void main(String [] args)
-{
-	System.out.println("Hello service");//serviceunivers
-	System.out.println("test");
-}
-	 
-}
+commence a reagadee
